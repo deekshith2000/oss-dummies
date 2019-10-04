8c7fcfbe5f97ffa06f9f756e17c49d7e6a80ab1c
deekshith2000 <deekshithgk2000@gmail.com>
2019-10-04 22:51:07 +0530

    add numbers

---
+++ py/chandan.py
@@ -0,0 +1,14 @@
+'''print("chandan")
+print("enter two values")
+x=int(input())
+y=int(input())
+z=x+y
+print(z)
+for i in range(10):
+    print(' '*n,end='')
+    print('* '*i)
+    n-=1
+for x in range(1,10):
+    for y in range(1,10):
+        print('{:6}'.format(x*y), end='')
+    print()'''
